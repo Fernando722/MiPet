9a107d52c0a2917eead779cc6fa4ccfa84353ad0
Luis Fernando Fernandez Muñoz <fercho722@gmail.com>
2018-11-26 21:42:22 -0500

    --Proyecto Inicial

--- MiPet/MiPet/App.xaml.cs
+++ MiPet/MiPet/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiPet.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace MiPet
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new TiendaPage());
         }
 
         protected override void OnStart()
